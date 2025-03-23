e966d3c1a6a37b63aea7a03dfe92707b8d5d0eb8
Dudley10 <cameron.dudley@bruins.belmont.edu>
2025-03-23 11:36:16 -0500

    Added art for characters
    made art from photoshop and imported them into unity

--- Assets/scripts/Invaders.cs
+++ Assets/scripts/Invaders.cs
@@ -18,16 +18,16 @@ public class Invaders : MonoBehaviour
     private void Awake(){
         for (int row = 0; row < this.rows; row++)
         {
-            float width = 2.0f * (this.columns - 1);
-            float height = 2.0f * (this.rows - 1);
+            float width = 3.0f * (this.columns - 1);
+            float height = 3.0f * (this.rows - 1);
             Vector2 centering = new Vector2(-width /2, -height / 2);
-            Vector3 rowPosition = new Vector3(centering.x, centering.y + (row * 2.0f), 0.0f);
+            Vector3 rowPosition = new Vector3(centering.x, centering.y + (row * 3.0f), 0.0f);
             for (int col = 0; col < this.columns; col ++)
             {
                 Invader invader = Instantiate(this.prefabs[row], this.transform);
                 invader.killed += InvaderKilled;
                 Vector3 position = rowPosition;
-                position.x += col * 2.0f;
+                position.x += col * 3.0f;
                 invader.transform.localPosition = position;
             }
         }
